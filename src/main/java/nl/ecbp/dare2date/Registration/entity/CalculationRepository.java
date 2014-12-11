8cc07998b47404e44a9e0b720f0de35dd3f83aab
maurits150 <public@maurits.tv>
2014-12-11 13:20:52 +0100

    Tests.

--- src/main/java/nl/ecbp/dare2date/Registration/entity/CalculationRepository.java
+++
@@ -1,6 +0,0 @@
-package nl.ecbp.dare2date.Registration.entity;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface CalculationRepository extends CrudRepository<Calculation, Long> {
-}
